05eef5c14c0bfe8462ebf6f2c1de3350a276cdbd
Matthew Vuong <matthewevuong@gmail.com>
2025-09-09 15:29:31 -0400

    done

--- cps109_lab1.py
+++ cps109_lab1.py
@@ -1,14 +1,12 @@
 #Assumed floats for most bc measurements / exact polynomials, took int as celsius
-#written by matthew vuong
-#student num 501348434
 
 #Question one
 print("Input a value in Celsius to convert into Fahrenheit: ")
-celc = int(input())
-x = round(((int)(celc)*(9/5)+32),2)
+cels = int(input())
+x = round(((int)(cels)*(9/5)+32),2)
 print("Converted Fahrenheit: "+str(x))
 
-#Question two //fix ts to not give cmplex
+#Question two 
 print("Input the first polynomial value: ")
 x = float(input())
 print("Input the second polynomial value: ")
@@ -20,11 +18,11 @@ root2 = format(((-y-(y**2-4*x*z)**0.5)/(2*x)),'.2f')
 print("the roots are: "+root+", "+root2)
 
 #Question three
-print("Input the first side: ")
+print("Input the first side of a triangle: ")
 a = float(input())
-print("Input the second side: ")
+print("Input the second side of a triangle: ")
 b = float(input())
-print("Input the third side: ")
+print("Input the third side of a triangle: ")
 c = float(input())
 print(a<=(b+c) and b<=(a+c) and c<=(a+b))
 
@@ -38,11 +36,13 @@ print("the area of your pentagon is: "+str(round((area),2)))
 print("Give me a number for the fibonacci sequence: ")
 n = int(input())
 gRatio = ((5)**0.5+1)/2
-fibbie = round(((2+gRatio)/5)*gRatio**(n)+((3-gRatio)/5)*gRatio**(-n)) #n minus 1 solved inaccuracy ??? idk ts buggy af
+fibbie = round(((2+gRatio)/5)*gRatio**(n)+((3-gRatio)/5)*gRatio**(-n))
 print("the 'n'th number of the fibonacci sequence is: "+str(fibbie))
-n=0
-print("Here is the first 10 numbers of the given formula \nThe inaccurate fibbie sequence: ")
-for i in range(10):
-    n+=1
-    fibbie = round(((2+gRatio)/5)*gRatio**(n)+((3-gRatio)/5)*gRatio**(-n)) #10 print
-    print(fibbie)
+
+
+#n=0
+#print("Here is the first 10 numbers of the given formula \nThe inaccurate fibbie sequence: ")
+#for i in range(10):
+#    n+=1
+#    fibbie = round(((2+gRatio)/5)*gRatio**(n)+((3-gRatio)/5)*gRatio**(-n)) #10 print
+#    print(fibbie)
