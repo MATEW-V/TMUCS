14ec9ba29de3366db7bd2a9653078848d23db681
Matthew Vuong <matthewevuong@gmail.com>
2025-09-09 10:17:27 -0400

    q3

--- cps109_lab1.py
+++ cps109_lab1.py
@@ -16,9 +16,12 @@ root2 = format(((-y-(y**2-4*x*z)**0.5)/2*x),'.2f')
 print("the roots are: "+str(root)+" "+str(root2))
 
 #Question three
-print("Input the first value: ")
+print("Input the first side: ")
 a = int(input())
-print("Input the second value: ")
+print("Input the second side: ")
 b = int(input())
-print("Input the third value: ")
+print("Input the third side: ")
 c = int(input())
+print(a>=(b+c) and (a+c)>=b and c >= (a+b))
+
+#Question 4
