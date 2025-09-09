a60ff3098696a466073a57709703d41c230db04b
Matthew Vuong <matthewevuong@gmail.com>
2025-09-09 11:13:54 -0400

    q4

--- cps109_lab1.py
+++ cps109_lab1.py
@@ -1,27 +1,39 @@
+#Assumed floats for most bc measurements / exact polynomials, took int as celsius
+
 #Question one
 print("Input a value in Celsius to convert into Fahrenheit: ")
-celc = input()
+celc = int(input())
 x = round(((int)(celc)*(9/5)+32),2)
 print("Converted Fahrenheit: "+str(x))
 
-#Question two
-print("Input the first value: ")
-x = int(input())
-print("Input the second value: ")
-y = int(input())
-print("Input the third value: ")
-z = int(input())
-root = format(((-y+(y**2-4*x*z)**0.5)/2*x),'.2f')
-root2 = format(((-y-(y**2-4*x*z)**0.5)/2*x),'.2f')
-print("the roots are: "+str(root)+" "+str(root2))
+#Question two //fix ts to not give cmplex
+print("Input the first polynomial value: ")
+x = float(input())
+print("Input the second polynomial value: ")
+y = float(input())
+print("Input the third polynomial value: ")
+z = float(input())
+root = format(((-y+(y**2-4*x*z)**0.5)/(2*x)),'.2f')
+root2 = format(((-y-(y**2-4*x*z)**0.5)/(2*x)),'.2f')
+print("the roots are: "+root+", "+root2)
 
 #Question three
 print("Input the first side: ")
-a = int(input())
+a = float(input())
 print("Input the second side: ")
-b = int(input())
+b = float(input())
 print("Input the third side: ")
-c = int(input())
-print(a>=(b+c) and (a+c)>=b and c >= (a+b))
+c = float(input())
+print(a<=(b+c) and b<=(a+c) and c<=(a+b))
 
 #Question 4
+print("Enter a side length for a pentagon: ")
+s = float(input())
+area = (1/4)*((5*(5+2*(5)**0.5))**0.5)*s**2
+print("the area of your pentagon is: "+str(round((area),2)))
+
+#Question 5
+print("Give me a number for the fibonacci sequence: ")
+n = int(input())
+fibbie = 1
+gRatio = 2
