bddc17cb0f5031c79a22b243c61e13ad738875f5
Matthew Vuong <matthewevuong@gmail.com>
2025-11-03 18:29:22 -0500

    did 7 more ilkka

--- labs109.py
+++ labs109.py
@@ -16,3 +16,64 @@ def ryerson_letter_grade(n):
     else:
         adjust = ""
     return "DCB"[tens - 5] + adjust
+
+def is_ascending(items):
+    x=True
+    for i in range(1,len(items)):
+        if items[i]<=items[i-1]:
+            return(False)
+        else:
+            pass
+    return(x)
+
+def riffle(items, out=True):
+    if out==True:
+        h1=items[:len(items)//2]
+        h2=items[len(items)//2:]
+    else:
+        h2=items[:len(items)//2]
+        h1=items[len(items)//2:]
+    shuffle=[]
+    for i in range(len(h1)):
+        shuffle.append(h1[i])
+        shuffle.append(h2[i])
+    return(shuffle)
+
+def only_odd_digits(n):
+    checka=True
+    for i in str(n):
+        if int(i)%2==0:
+            checka=False
+    return(checka)
+
+def is_cyclops(n):
+    x=str(n)
+    if x=="0":
+        return(True)
+    if "0" in x and x.count("0")==1 and x[len(x)//2]=="0" and x[-1]!="0" and len(x)%2!=0:
+        return(True)
+    else:
+        return(False)
+    
+def domino_cycle(tiles):
+    if not tiles:
+        return(True)
+    if len(tiles) == 1:
+        return tiles[0][0] == tiles[0][1]
+
+    return all(tiles[i][1] == tiles[i + 1][0] for i in range(len(tiles) - 1)) and tiles[0][0] == tiles[-1][1]
+#redo this stupid
+
+def colour_trio(colors):
+    cset = {"y", "r", "b"}
+    x = list(colors)
+    while len(x) > 1:
+        new_x = []
+        for i in range(1,len(x)):
+            if x[i] == x[i-1]:
+                new_x.append(x[i])
+            else:
+                new_x.append((cset - {x[i], x[i-1]}).pop())
+        x = new_x
+
+    return x[0]
