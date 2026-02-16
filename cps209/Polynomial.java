cbebc7ffb9f87d854319fdabe06ea548e8e5a435
Matthew <Matthewevuong@gmail.com>
2026-02-15 23:00:27 -0500

    lab4 done

--- cps209/Polynomial.java
+++ cps209/Polynomial.java
@@ -1,22 +1,9 @@
 
 public class Polynomial 
 {
-
-    /*
-        Fill in the methods below according to the descriptions given
-        in the lab document. See the PDF in this archive.
-
-        To test your code, you can scroll down to the very bottom of this 
-        file for instructions on how to run the unit tests. 
-
-        Note that your official lab grade will be based on the proportion
-        of unit tests that you pass.    
-    */
-
     private final int[] coeff;
     
-    public Polynomial(int[] coefficients) 
-    {   
+    public Polynomial(int[] coefficients) {   
         int last = -1;
         for (int i=coefficients.length-1; i>=0; i--) {
             if (coefficients[i]!=0) {
@@ -34,13 +21,11 @@ public class Polynomial
         }
     }
     
-    public int getDegree() 
-    {
+    public int getDegree() {
         return coeff.length-1;
     }
     
-    public int getCoefficient(int k) 
-    {
+    public int getCoefficient(int k) {
         if (k<0) {
             return 0;
         } else if (k<=coeff.length-1) {
@@ -50,31 +35,64 @@ public class Polynomial
         }
     }
 
-    public long evaluate(int x) {
-        int cdeg = getDegree();
-        int eval = 0;
-        for (int i=0; i<coeff.length-1; i++) {
-            eval += getCoefficient(i)*x**(cdeg-i);
+    public static int powerLoop(int base, int exponent) {
+        int result = 1;
+        if (exponent == 0) {
+            return 1;
         }
-        return -1L;
+        for (int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+        return result;
+    }
+
+    public long evaluate(int x) {
+        long eval = 0;
+        for (int i=0; i<coeff.length; i++) {
+            eval += getCoefficient(i)*(powerLoop(x, i));
+        }
+        return eval;
     }
     
     public Polynomial add(Polynomial other) {
-        return null;
+        int maxDegree = Math.max(this.getDegree(), other.getDegree());
+        int[] res = new int[maxDegree + 1];
+        
+        for (int i = 0; i <= maxDegree; i++) {
+            int value1 = this.getCoefficient(i);
+            int value2 = other.getCoefficient(i);
+            res[i] = value1 + value2;
+        }
+        return new Polynomial(res);
     }
-
     @Override
     public String toString() {
-        return "";
+        StringBuilder sb = new StringBuilder();
+        int degree = getDegree();
+
+        for (int i = degree; i >= 0; i--) {
+            int coeffS = getCoefficient(i); 
+            if (coeffS == 0) continue;
+
+            if (sb.length() > 0) {
+                sb.append(coeffS > 0 ? " + " : " - ");
+            } else if (coeffS < 0) {
+                sb.append("-");
+            }
+            int absCoeff = Math.abs(coeffS);
+
+            if (i == 0) {
+                sb.append(absCoeff);  // Always show constant term coefficient
+            } else if (i == 1) {
+                sb.append(absCoeff + "x");  // Always show coefficient for linear term
+            } else {
+                sb.append(absCoeff + "x^" + i);  // Always show coefficient for higher terms
+            }
+        }
+        return sb.length() == 0 ? "0" : sb.toString();
     }
     /* RUNNING THE UNIT TESTS)
-     *
-     * Evaluate your code by using the following commands in the terminal.
-     * Any time you make changes to your code, you will have to run 
-     * both commands again. The first one compiles your code and the 
-     * tester into two Java class files. The second command runs the
-     * unit tests and displays the results.
-     *
+    
      * Compile)
      *  javac -cp junit-1.11.0-M2.jar Polynomial.java PolynomialTestOne.java
      *
@@ -82,6 +100,5 @@ public class Polynomial
      *  java -jar junit-1.11.0-M2.jar execute -cp . -c PolynomialTestOne
      *
      * When all test report a pass, you're good to go.
-     *  
      */
 }
